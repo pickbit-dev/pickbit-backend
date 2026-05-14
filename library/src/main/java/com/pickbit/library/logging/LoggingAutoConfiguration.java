c326603aafccfa4ce67bae86aa0c7b9100db3c14
jonghun <whdgns4288@naver.com>
2026-05-14 09:19:09 +0900

    feat: auto-configure service request logging

--- library/src/main/java/com/pickbit/library/logging/LoggingAutoConfiguration.java
+++ library/src/main/java/com/pickbit/library/logging/LoggingAutoConfiguration.java
@@ -1,30 +1,40 @@
-package com.pickbit.productservice.config;
+package com.pickbit.library.logging;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.pickbit.library.logging.config.FilterProperties;
 import com.pickbit.library.logging.logger.LoggingFilter;
 import com.pickbit.library.logging.logger.RequestLogger;
 import com.pickbit.library.logging.logger.ResponseLogger;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
+@AutoConfiguration
+@ConditionalOnWebApplication(type = ConditionalOnWebApplication.Type.SERVLET)
 @EnableConfigurationProperties(FilterProperties.class)
-public class LoggingConfig {
+public class LoggingAutoConfiguration {
 
     @Bean
+    @ConditionalOnMissingBean
     public RequestLogger requestLogger(ObjectMapper objectMapper) {
         return new RequestLogger(objectMapper);
     }
 
     @Bean
+    @ConditionalOnMissingBean
     public ResponseLogger responseLogger(ObjectMapper objectMapper) {
         return new ResponseLogger(objectMapper);
     }
 
     @Bean
-    public LoggingFilter loggingFilter(RequestLogger requestLogger, ResponseLogger responseLogger, FilterProperties filterProperties) {
+    @ConditionalOnMissingBean
+    public LoggingFilter loggingFilter(
+            RequestLogger requestLogger,
+            ResponseLogger responseLogger,
+            FilterProperties filterProperties
+    ) {
         return new LoggingFilter(requestLogger, responseLogger, filterProperties);
     }
 }
